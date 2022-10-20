3e80d4fbb7e4af5ae0988ca6545ae402806f2735
Gotsuiwashi <modojin4@gmail.com>
2022-10-20 01:41:22 +0000

    Facade is developed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,31 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: "Dev Blog",
+    description: "Gatsbyで作成したブログサイトです。",
+    author: "Engineer X"
+  },
+  plugins: [
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_DELIVERY_ACCESS_TOKEN
+      }
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        commonmark: true,
+        footnotes: true,
+        pedantic: true,
+        gfm: true,
+        plugins: [],
+      },
+    }
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,44 @@
+import { graphql } from "gatsby"
 import React from "react"
+import ContactForm from "../components/contact-form"
+import Hero from "../components/hero"
+import Layout from "../components/layout"
+import Postlink from "../components/post-link"
+import Seo from "../components/seo"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Home({ data }) {
+  return (
+    <Layout>
+      <Seo title="Dev Blog" description="Gatsbyを使って作ったブログです" />
+      <Hero></Hero>
+      <div>Hello blogs</div>
+      {data.allContentfulPost.edges.map(edge =>
+        <Postlink key={edge.node.slug} post={edge.node} />
+      )}
+      <ContactForm></ContactForm>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+    query  {
+      allContentfulPost {
+        edges {
+          node {
+            title
+            image {
+              title
+              file {
+                url
+              }
+            }
+            description {
+              description
+            }
+            slug
+            updatedAt(locale: "ja-JP", formatString: "YYYY年MM月DD日")
+          }
+        }
+      }
+    }
+`
